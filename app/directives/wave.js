c64de624d160f2ffbba52d497bd19aa7be17d80e
wysiwyg826 <wysiwyg826@gmail.com>
2016-10-10 22:31:18 +0800

    修改wave指令，使其可连续点击。

--- app/directives/wave.js
+++ app/directives/wave.js
@@ -4,7 +4,7 @@
  * @Date:    2016-10-10 09:32:45
  * @File:    wave.js
  * @Last modified by:   wysiwyg826
- * @Last Modified time: 2016-10-10 14:33:21
+ * @Last Modified time: 2016-10-10 22:30:36
  * @Description: 
  */
 'use strict';
@@ -13,38 +13,37 @@ import Vue from 'vue';
 
 Vue.directive('wave', {
   bind: function(el, binding, vnode) {
-    let once = false
+
+  },
+  inserted: function(el, binding, vnode) {
+    const [offsetLeft, offsetTop] = [el.offsetLeft, el.offsetTop];
     el.addEventListener('click', function(e) {
-      e.preventDefault();
-      if (!e) return false;
-      if (once) return false;
-      once = true;
-      let [x, y, l] = [e.offsetX, e.offsetY, 0];
+      let [x, y] = [(e.clientX - offsetLeft), (e.clientY - offsetTop)];
       let ele = document.createElement("div");
+      let scale = 1;
       ele.style.position = "absolute";
-      ele.style.height = l + "px";
-      ele.style.width = l + "px";
+      ele.style.height = "15px";
+      ele.style.width = "15px";
       ele.style.left = x + "px";
       ele.style.top = y + "px";
       ele.style.backgroundColor = "rgba(255, 255, 255, .2)";
       ele.style.display = "block";
       ele.style.borderRadius = "1000px";
+      ele.style.transform = "scale(1,1)";
       el.appendChild(ele);
 
       let timer = setInterval(function() {
-        x -= 3;
-        y -= 3;
-        l += 6;
-        ele.style.left = x + "px";
-        ele.style.top = y + "px";
-        ele.style.height = l + "px";
-        ele.style.width = l + "px";
-        if(l > 200) {
-          once = false
+        scale += 0.5;
+        ele.style.transform = "scale(" + scale + "," + scale + ")";
+
+        if (scale > 20) {
           clearInterval(timer);
           el.removeChild(ele);
         }
       }, 16.7);
-    }, true);
+    });
+  },
+  update: function(el, binding, vnode) {
+    console.log(el);
   }
 });
